6129b094d46df951c23849d2ced1dd2186acfd5d
S2KB4X <Pavel_Yeremeev@swissre.com>
2022-05-03 14:34:16 +0300

    Login test cases introduction

---
+++ Cypress/cypress/integration/specs/intranetLogin.spec.ts
@@ -0,0 +1,35 @@
+describe('Intranet Test', () => {
+
+    it.only('Login happyPath', () => {
+        cy.visit(Cypress.env('baseUrl'));
+        cy.get('#username').type(Cypress.env('usar'));
+        cy.get('#password').type(Cypress.env('pazz'), { log: false });
+        cy.contains('Login').should('be.visible').click();
+        cy.get('#msg.errors').should('not.exist');
+        cy.get('#profile > .avatar-block').should('be.visible').click();
+
+        const userName = Cypress.env('usar').replace('.', "-");
+        cy.get('.user-nicename').contains(userName).should((elem) => {
+            expect(elem.text()).to.equal('@' + userName);
+        });
+    });
+
+    it('Login failed', () => {
+        cy.visit(Cypress.env('baseUrl'));
+        cy.get('#username').type(Cypress.env('usar'));
+        cy.get('#password').type('123');
+        cy.contains('Login').should('be.visible').click();
+        cy.get('#msg.errors').should('be.visible').contains('Invalid credentials.');
+        });
+
+    it('Logout after login', () => {
+        cy.visit(Cypress.env('baseUrl'));
+        cy.get('#username').type(Cypress.env('usar'));
+        cy.get('#password').type(Cypress.env('pazz'), { log: false });
+        cy.contains('Login').should('be.visible').click();
+        cy.get('.logout-button').should('be.visible').click();
+        cy.get('#logout').should('be.visible');
+        cy.get('#logout > #msg.success > h2').should('be.visible').contains('Logout successful');
+        cy.get('#logout > #msg.success > p').should('be.visible').contains('For security reasons, exit your web browser.');
+        });
+});
